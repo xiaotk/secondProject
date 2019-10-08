0dced2e463ab9be6c066839fd8b8d528aa50a343
JY <1241924307@qq.com>
2019-10-08 13:51:23 +0800

    项目第二次修改

--- app/src/main/java/com/example/new_app2/MainActivity.java
+++ app/src/main/java/com/example/new_app2/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.new_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+//   按钮命名
+    private Button btn_register;
+    private Button btn_sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register);
+//  寻找按钮
+        btn_register = (Button)findViewById(R.id.register);
+        btn_sign = (Button) findViewById(R.id.sign_up);
+//  对注册按钮触发点击事件实现activity跳转
+        btn_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+            }
+        });
     }
 }
